6a1db536b26c97961c12b59584b061ffe095b621
Renzo Trabucco <renzotrabuccolissarrague@gmail.com>
2024-07-30 22:55:29 -0300

    quotes

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -6,7 +6,7 @@ const fetchQuote = async () => {
     const res  = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
     const data = await res.json();
     console.log(data[0]);
-    return data;
+    return data[0];
     
 }
 
@@ -17,8 +17,26 @@ const fetchQuote = async () => {
 export const BreakingbadApp = async ( element ) => {
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
-    const quote = await fetchQuote();
-    element.innerHTML = 'Tenemos data !!';
+    //await fetchQuote();
+    
+    const quoteLabel      = document.createElement('blockquote');
+    const authorLabel     = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authorLabel, nextQuoteButton );
+    }   
+
+    nextQuoteButton.addEventListener('click', async () => {
+        element.innerHTML = 'Loading...';
+        const quote = await fetchQuote();
+        renderQuote( quote );
+    })
 
 
+    fetchQuote()
+        .then( renderQuote );
 }
